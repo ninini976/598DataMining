6520b319308857f341bcd8bd574f61c83b2c5868
Fan Yang <yangtony@Fans-MacBook-Pro.local>
2019-04-17 18:46:22 -0400

    automate script run.sh in gcn

--- 598Project/run.sh
+++ 598Project/run.sh
@@ -1,4 +1,4 @@
 python f.py
-rm ../../GCN/gcn/gcn/data/ind.BuzzFeedSample*
-cp ind.BuzzFeedSample* ../../GCN/gcn/gcn/data
-python ../../GCN/gcn/gcn/train.py
+rm ../GCN/gcn/gcn/data/ind.BuzzFeedSample*
+cp ind.BuzzFeedSample* ../GCN/gcn/gcn/data
+python ../GCN/gcn/gcn/train.py --dataset BuzzFeedSample182
